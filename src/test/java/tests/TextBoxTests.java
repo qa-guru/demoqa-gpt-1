843bb0e8a24313487a0f388610de11346fff1aea
svasenkov <aanher@gmail.com>
2024-03-02 12:51:24 +0100

    added Meta-data and steps

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,34 +1,40 @@
 package tests;
 
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
+@Feature("Demo text box")
 public class TextBoxTests extends TestBase {
 
     @Test
+    @DisplayName("Text box can be filled")
     void fillFormTest() {
-        open("/text-box");
+        step("Open page /text-box", () -> {
+            open("/text-box");
 
-        if($(".fc-dialog-content").isDisplayed())
-            $(byText("Consent")).click();
-
-        $("#userName").setValue("Alex");
-        $("#userEmail").setValue("alex@egorov.com");
-        $("#currentAddress").setValue("Some street 1");
-        $("#permanentAddress").setValue("Another street 1");
-        $("#submit").click();
-
-        $("#output #name").shouldHave(text("Alex"));
-        $("#output #email").shouldHave(text("alex@egorov.com"));
-        $("#output #currentAddress").shouldHave(text("Some street 1"));
-        $("#output #permanentAddress").shouldHave(text("Another street 1"));
+            if($(".fc-dialog-content").isDisplayed())
+                $(byText("Consent")).click();
+        });
+        step("Fill form", () -> {
+            $("#userName").setValue("Alex");
+            $("#userEmail").setValue("alex@egorov.com");
+            $("#currentAddress").setValue("Some street 1");
+            $("#permanentAddress").setValue("Another street 1");
+            $("#submit").click();
+        });
+        step("Check results", () -> {
+            $("#output #name").shouldHave(text("Alex"));
+            $("#output #email").shouldHave(text("alex@egorov.com"));
+            $("#output #currentAddress").shouldHave(text("Some street 1"));
+            $("#output #permanentAddress").shouldHave(text("Another street 1"));
+        });
     }
 }
