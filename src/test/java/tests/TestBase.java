3449cdae35b5c065ab023b582583a4b0e51aa361
svasenkov <aanher@gmail.com>
2024-03-02 12:47:37 +0100

    fix video

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,16 +4,26 @@ import com.codeborne.selenide.Configuration;
 import helpers.Attach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.timeout = 5000; // default 4000
 //        Configuration.pageLoadStrategy = "eager";
 //        Configuration.holdBrowserOpen = true;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        Configuration.timeout = 5000; // default 4000
     }
 
     @AfterEach
