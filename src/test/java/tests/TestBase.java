e749afdb5042f5732ab55fa4835254efed14d32d
svasenkov <aanher@gmail.com>
2024-03-02 12:55:45 +0100

    added selenide logging

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,6 +3,7 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
