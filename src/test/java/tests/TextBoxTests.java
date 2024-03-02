9c8020c2a7bdd10bddd13e32dae8bd67e75cbcf3
svasenkov <aanher@gmail.com>
2024-03-02 12:35:04 +0100

    added Selenoid

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -15,8 +15,9 @@ public class TextBoxTests {
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+//        Configuration.pageLoadStrategy = "eager";
+//        Configuration.holdBrowserOpen = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         Configuration.timeout = 5000; // default 4000
     }
 
